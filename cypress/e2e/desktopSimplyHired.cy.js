4516fc578514684c426baa54a3cb974cf8204e6d
Aldrine John Reynon <aldrine.reynon@gmail.com>
2023-05-26 14:50:58 +0800

    Added desktop testscript for simplyhired website

---
+++ cypress/e2e/desktopSimplyHired.cy.js
@@ -0,0 +1,26 @@
+describe('Desktop View', function()
+{
+    it('Narrow search job results', function()
+    {
+        cy.visit('/')
+        cy.get('[data-testid="findJobsKeywordInput"]').type('Javascript React')
+        cy.get('[data-testid="findJobsLocationInput"]').clear().type('New York City')
+        cy.get('[data-testid="findJobsSearchSubmit"]').click()
+        cy.wait(3000)
+        cy.get('[data-testid="dropdown"]').children('span').contains('Job Type').click({force:true})
+        cy.get('[data-testid="dropdown-option"]').children('span').contains('Full-time').click()
+        cy.wait(5000)
+        cy.get('[data-testid="dropdown"]').children('span').contains('Minimum Salary').click({force:true})
+        cy.get('[data-testid="dropdown-option"]').children('span').contains('$105,000').click()
+        cy.wait(5000)
+        cy.get('.chakra-radio').children('span').contains('Date').click({force:true})
+        cy.wait(5000)
+        cy.get('#job-list').within(() => {
+            for(var list = 0; list < 5; list++) 
+            {
+                cy.get('li').eq(list).click()
+                cy.wait(2000)
+            }
+        })
+    })
+})
